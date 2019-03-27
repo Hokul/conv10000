dc4632c93d10b9b46d75b9c0353943b6d1f54fe8
Hokul <46651524+Hokul@users.noreply.github.com>
2019-03-27 23:26:08 +0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,6 +1,5 @@
 use std::io;
 
-
 fn main() {
     let mass_base_pair: f64 = 617.96;
     let mass_base: f64 = mass_base_pair / 2_f64;
@@ -8,17 +7,15 @@ fn main() {
 
     loop {
     	println!("Please choose dsDNA(1) or ssDNA(2)! Input 3 or more if you're want to finish.");
-
     	let mut start_number = String::new();
     	io::stdin().read_line(&mut start_number)
         	.expect("Failed to read line");
-	    let start_number: u64 = start_number.trim().parse()
-    	    .expect("Please type a number!");
+	let start_number: u64 = start_number.trim().parse()
+    		.expect("Please type a number!");
 
     	if start_number > 2 {
     		println!("Thank you for using this programm!");
     		break;
-
     	} else {
     		println!("What is the sequence lenght (bp)?");
     		let mut lenght_of_seq = String::new();
@@ -38,18 +35,16 @@ fn main() {
     		if start_number == 1 {
     			let molecular_weight = lenght_of_seq * mass_base_pair + 36.04;
 
-    			let mol = ((weight_of_seq * 1e-9_f64) / molecular_weight) * 1e12_f64;
-
-       			let copy_number = mol * 1e-12_f64 * avogadro_constant;
+       			let copy_number = (((weight_of_seq * 1e-9_f64) / molecular_weight) * 1e12_f64) * 1e-12_f64 * avogadro_constant;
     			println!("DNA copy number: {} into {}ng",  copy_number as u64, weight_of_seq as u64);
 
     			let wishes_weight = wishes_copy_seq / avogadro_constant * molecular_weight * 1e9_f64;
     			println!("Weight of {} DNA copies it is {} ng", wishes_copy_seq as u64, wishes_weight);
 
     			let mut water_volume = weight_of_seq/wishes_weight;
-
     			println!("You need to use {} µl of water to dissolve DNA", water_volume as u64);
     			println!("");
+			
     			let mut counter = 1_u8;
     			while water_volume > 1000_f64 {
     				water_volume = water_volume / 1000_f64;
@@ -61,21 +56,19 @@ fn main() {
     				counter = counter +1;
     			}
     			println!("{}. Add {} µl of water into 1 µl DNA", counter, water_volume as u64 - 1);
-
     		} else {
     			let molecular_weight = lenght_of_seq * mass_base + 18.02;
 
-    			let mol = ((weight_of_seq * 1e-9_f64) / molecular_weight) * 1e12_f64;
-
-       			let copy_number = mol * 1e-12_f64 * avogadro_constant;
+       			let copy_number = (((weight_of_seq * 1e-9_f64) / molecular_weight) * 1e12_f64) * 1e-12_f64 * avogadro_constant;
     			println!("DNA copy number: {} into {}ng.",  copy_number as u64, weight_of_seq as u64);
 
     			let wishes_weight = wishes_copy_seq / avogadro_constant * molecular_weight * 1e9_f64;
     			println!("Weight of {} DNA copies it is {} ng", wishes_copy_seq as u64, wishes_weight);
-    			let mut water_volume = weight_of_seq/wishes_weight;
-
+    			
+			let mut water_volume = weight_of_seq/wishes_weight;
     			println!("You need to use {} µl of water to dissolve DNA.", water_volume as u64);
     			println!("");
+			
     			let mut counter = 1_u8;
     			while water_volume > 1000_f64 {
     				water_volume = water_volume / 1000_f64;
@@ -87,19 +80,9 @@ fn main() {
     				counter = counter +1;
     			}
     			println!("{}. Add {} µl of water into 1 µl DNA", counter, water_volume as u64 - 1);
-
-
     		}
 
     	}
-
-
-
-
-
-
-
-
    	}
 }
 
